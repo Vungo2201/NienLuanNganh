5063c3df73e188aeb774c794b0c7edebf0d7a982
Vungo2201 <vu22012002@gmail.com>
2023-10-30 17:14:48 +0700

    Tao camera player

--- level.py
+++ level.py
@@ -6,6 +6,8 @@ class Level:
     def __init__(self,level_data,surface):
         self.display_surface = surface
         self.set_level(level_data)
+        self.world_shift = 0
+        self.current_x = 0
         
     def set_level(self,layout):
         self.tiles = pygame.sprite.Group()
@@ -22,11 +24,20 @@ class Level:
                 if cell == 'P':
                     player_sprite = Player((x,y))
                     self.player.add(player_sprite)
+    
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+        
+        if player_x < 200:
+            self.world_shift = 8
+            player.speed = 0
         
     def run(self):
-        self.tiles.update(0)
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         
         self.player.update()
         self.player.draw(self.display_surface)
-        
+        self.scroll_x()
