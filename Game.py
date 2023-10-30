2b1402c38553e5fbb9e1b1fdc44f4db8ca49a74e
Vungo2201 <vu22012002@gmail.com>
2023-10-30 16:51:31 +0700

    Khoi tao level test, player test

--- Game.py
+++ Game.py
@@ -1,21 +1,26 @@
 import pygame as pg
 import sys
+from setting import *
+from level import Level
 
 class Game:
     def __init__(self):
-        screen_width = 900
+        screen_width = 1250
         screen_heigh = 600
         pg.init()
         pg.display.set_caption("Pixel Adventure")
         self.screen = pg.display.set_mode((screen_width,screen_heigh))
         self.clock = pg.time.Clock()
+        self.level = Level(level_map,self.screen)
     def run(self):
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
-
+                    
+            self.screen.fill('black')
+            self.level.run()
             pg.display.update()
             self.clock.tick(60)
 
---
+++ level.py
@@ -0,0 +1,32 @@
+import pygame 
+from tiles import Tile 
+from setting import  tile_size
+from player import Player
+class Level:
+    def __init__(self,level_data,surface):
+        self.display_surface = surface
+        self.set_level(level_data)
+        
+    def set_level(self,layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
+        for row_index,row in enumerate(layout):
+            for col_index,cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+				
+                if cell == 'X':
+                    tile = Tile((x,y),tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x,y))
+                    self.player.add(player_sprite)
+        
+    def run(self):
+        self.tiles.update(0)
+        self.tiles.draw(self.display_surface)
+        
+        self.player.update()
+        self.player.draw(self.display_surface)
+        
